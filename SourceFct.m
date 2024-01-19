9f03471577ebdf14072ac31bc71cde266562f53c
OKruegs <owen-krueger@hotmail.com>
2024-01-19 17:26:11 -0500

    Milestone 1 Code Done

--- SourceFct.m
+++ SourceFct.m
@@ -1,14 +1,14 @@
 function E = SourceFct(t,InputParas)
 
-if isfield(InputParas,'rep')
+if isfield(InputParas,'rep') %if rep is a name of field InputParas
     n = floor(t/InputParas.rep);
     t = t-n*InputParas.rep;
 end
 
-if ~isstruct(InputParas)
-    E = InputParas;
+if ~isstruct(InputParas) %if InputParas is not struct
+    E = InputParas; 
 else
-    E = InputParas.E0*exp(-(t-InputParas.t0)^2/InputParas.wg^2)*exp(1i*(InputParas.we*t + InputParas.phi));
+    E = InputParas.E0*sin(-(t-InputParas.t0)^2/InputParas.wg^2)*sin(1i*(InputParas.we*t + InputParas.phi));
 end
 
 end
